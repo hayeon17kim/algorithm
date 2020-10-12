6960f5bbe2db017dbea8a9244ea1396ef7cc0286
hayeon17kim <qwndhs@gmail.com>
2020-10-13 00:00:46 +0900

    doit

--- src/main/java/algorithm/doit/ch03/Q04.java
+++ src/main/java/algorithm/doit/ch03/Q04.java
@@ -4,74 +4,73 @@ import java.util.Scanner;
 
 public class Q04 {
 
-  static int binSearchEx(int[] x, int num, int key) {
-
+  // 배열 a의 앞쪽 n개 요소에서 key와 같은 요소를 선형 검색 (보초법)
+  static int binSearchEx(int[] a, int n, int key) {
     System.out.print("   |");
-    for (int k = 0; k < num; k++)
+    for (int k = 0; k < n; k++)
       System.out.printf("%4d", k);
     System.out.println();
 
     System.out.print("---+");
-    for (int k = 0; k < 4 * num + 2; k++)
+    for (int k = 0; k < 4 * n + 2; k++)
       System.out.print("-");
     System.out.println();
 
-    int pl = 0;
-    int pr = num - 1;
+    int pl = 0; // 검색범위 맨 앞의 index
+    int pr = n - 1; // 검색범위 맨 뒤의 index
 
     do {
-      int pc = (pr + pl)/2;
+      int pc = (pl + pr) / 2; // 중앙요소의 index
       System.out.print("   |");
-      // 공백을 얼마나 줘야 하는 지에 대한 것임. 뒤에 빈 문자열은 공백을 위한 것
       if (pl != pc)
         System.out.printf(String.format("%%%ds<-%%%ds+", (pl * 4) + 1, (pc - pl) * 4), "", "");
       else
-        System.out.printf(String.format("%%%ds+", pc * 4 + 1), "");
-      
+        System.out.printf(String.format("%%%ds<-+", pc * 4 + 1), "");
       if (pc != pr)
         System.out.printf(String.format("%%%ds->\n", (pr - pc) * 4 - 2), "");
       else
         System.out.println("->");
       System.out.printf("%3d|", pc);
-      for (int k = 0; k < num; k++)
-        System.out.printf("%4d", x[k]);
-      System.out.println();
-      if (x[pc] == key)
-        return pc;
-      else if (x[pc] > key)
-        pr = pc - 1;
-      else 
-        pl = pc + 1;
-
+      for (int k = 0; k < n; k++)
+        System.out.printf("%4d", a[k]);
+      System.out.println("\n   |");
+      if (a[pc] == key)
+        return pc; // 검색성공
+      else if (a[pc] < key)
+        pl = pc + 1; // 검색범위를 뒤쪽 절반으로 좁힘
+      else
+        pr = pc - 1; // 검색범위를 앞쪽 절반으로 좁힘
     } while (pl <= pr);
-
-    return -1;
+    return -1; // 검색실패
   }
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("요솟수: ");
-    int num = sc.nextInt();
-    int[] x = new int[num];
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.print("요솟수：");
+    int num = stdIn.nextInt();
+    int[] x = new int[num]; // 요솟수 num인 배열
 
     System.out.println("오름차순으로 입력하세요.");
-    x[0] = sc.nextInt();
+
+    System.out.print("x[0]："); // 맨 앞 요소를 읽어 들임
+    x[0] = stdIn.nextInt();
 
     for (int i = 1; i < num; i++) {
       do {
-        System.out.print("x[" +  i + "]:");
-        x[i] = sc.nextInt();
+        System.out.print("x[" + i + "]：");
+        x[i] = stdIn.nextInt();
       } while (x[i] < x[i - 1]); // 하나 앞의 요소보다 작으면 다시 입력
     }
 
-    System.out.print("찾는 값: ");
-    int key = sc.nextInt();
+    System.out.print("찾는 값："); // 키 값을 입력 받음
+    int ky = stdIn.nextInt();
 
-    int idx = binSearchEx(x, num, key);
+    int idx = binSearchEx(x, num, ky); // 배열 x에서 값이 ky인 요소를 검색
 
     if (idx == -1)
-      System.out.println("그 값의 요소가 없습니다.");      
+      System.out.println("그 값의 요소가 없습니다.");
     else
-      System.out.println(key + "은  x[" + idx + "]에 있습니다.");
+      System.out.println(ky + "은 x[" + idx + "]에 있습니다.");
   }
 }
