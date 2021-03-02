efee1b350679598e99a273813a53e1872a899bd7
hayeon17kim <qwndhs@gmail.com>
2021-03-03 01:40:00 +0900

    algorithm: meeting room

--- src/main/java/algorithm/top/MeetingRoom.java
+++ src/main/java/algorithm/top/MeetingRoom.java
@@ -1,20 +1,40 @@
 package algorithm.top;
 
-class Interval{
-  int start;
-  int end;
-  Interval(){
-    this.start = 0;
-    this.end =0;
+public class MeetingRoom {
+  static class Interval{
+    int start;
+    int end;
+    Interval(){
+      this.start = 0;
+      this.end =0;
+    }
+    Interval(int s, int e){
+      this.start = s;
+      this.end = e;
+    }
   }
-  Interval(int s, int e){
-    this.start = s;
-    this.end = e;
+
+  public static void main(String[] args) {
+    System.out.println(mySolution(new Interval[] {new Interval(0,30), new Interval(5,10), new Interval(15, 20)}));
+    System.out.println(mySolution(new Interval[] {new Interval(7,10), new Interval(2,4)}));
+    
   }
-}
-public class MeetingRooms {
-  public boolean solve(Interval[] intervals) {
-    return false;
+
+  public static boolean mySolution(Interval[] intervals) {
+    int begin = intervals[0].start;
+    int finish = intervals[0].end;
+    boolean[] time = new boolean[100000];
+    for (int i = 0; i < intervals.length; i++) {
+      /*
+      if (begin > intervals[0].start) begin = intervals[i].start;
+      if (finish < intervals[0].end) finish = intervals[i].end;
+       */
+      for (int j = intervals[i].start; j < intervals[i].end; j++) {
+        if (!time[j]) time[j] = true;
+        else return false;
+      }
+    }
+    return true;
   }
 }
 
